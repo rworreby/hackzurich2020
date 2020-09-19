49ca220d0b34fbd23a31903f572856926603aa07
cmitsch <christian.mitsch@disneyresearch.com>
2020-09-19 17:50:25 +0200

    Working version of pickup endpoints

--- app/api.py
+++ app/api.py
@@ -12,25 +12,42 @@ bp = Blueprint('api', __name__, url_prefix="/api")
 @bp.route('/pickups', methods=['GET'])
 def index2():
     db = get_db()
-    return jsonify(db.executescript("SELECT * FROM pickup").fetchall())
+    return jsonify(db.execute("SELECT * FROM pickup").fetchall())
 
 #add a new pick-up
 @bp.route('/pickups', methods=['POST'])
 def index3():
     error = None
     if (request.method == 'POST'):
-        driverID = request.form['driverID']
+            
+        db = get_db()
+
+        db.execute("INSERT INTO pickup VALUES (NULL,?,?,?,?,?,?,?)", 
+        (int(request.form['truckID']), 
+        request.form['notes'], 
+        request.form['type'],
+        float(request.form['startLat']),
+        float(request.form['startLog']),
+        float(request.form['endLog']),
+        float(request.form['endLat'] )
+        ))
+
+        db.commit()
     
+        return jsonify ({'status' : 'success'})
+    else:
+        return jsonify({'status' : 'failed'})
+
+
+@bp.route('/pickups/<int:id>', methods=['GET'])
+def index4(id):
     db = get_db()
-
-    db.execute("INSERT INTO pickup VALUES (NULL,?,?,?,?,?,?,?)", (32, 'notes', 'type', 4.3, 2.1, 2.1, 1.5))
-    db.commit()
     
-    return "Add a new pick-up with ID " + str(driverID)
-
-
-    
-
+    numEntries = int(db.execute("SELECT COUNT(*) FROM pickup").fetchone()['COUNT(*)'])
+    if (id > numEntries):
+        return jsonify ({'status': 'failed'})
+    else:
+        return jsonify(db.execute("SELECT * FROM pickup WHERE id=" + str(id)).fetchall())
 
 
 
