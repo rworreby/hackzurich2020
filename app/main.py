d7d21fdf0624d3af215aadb684ae41e347684457
Robin Worreby <rworreby@student.ethz.ch>
2020-09-19 15:51:08 +0200

    Add redirect for index page to main

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
-
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from app.db import get_db
 
 bp = Blueprint('main', __name__, url_prefix="")
+
+
+@bp.route('/')
+def index():
+    return redirect('main')
+
 
 @bp.route('/main', methods=['GET'])
 def register():
