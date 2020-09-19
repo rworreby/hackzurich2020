c0d8ad1cb153ef5d554de81a69a99d996d6e9ae8
Robin Worreby <rworreby@student.ethz.ch>
2020-09-19 13:27:39 +0200

    Add basic Flask backend routing logic

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('main'))
+
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
